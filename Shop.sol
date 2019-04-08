e9c91d0d0f5f1429ca2df5a8ea06f34c6751d424
Connor O'Hara <connor@s1nus.com>
2019-04-08 16:02:55 -0400

    updated pragma

--- Shop.sol
+++ Shop.sol
@@ -1,24 +1,24 @@
-pragma solidity ^0.4.22;
+pragma solidity >=0.4.22 <0.6.0;
 
 contract Shop {
-    
+
     address property_manager;
     mapping (address => bool) public employees;
     uint min_balance = 71;
     uint lemonade_price = 50;
     uint balance;
-    
+
     constructor() public {
         property_manager = msg.sender;
     }
-    
+
     modifier onlyEmployee() {
         require(employees[msg.sender] == true);
         _;
     }
-    
-    function buyLemonade() payable {
-        balance += msg.balance;
+
+    function buyLemonade() public payable {
+        balance += msg.value;
     }
-    
+
 }
