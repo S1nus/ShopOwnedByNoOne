10d9b22bf45a73ce02a0d2cc2eed83b6747b9ec4
Connor O'Hara <connor@s1nus.com>
2019-04-14 17:23:02 -0400

    debugging pay_employees

--- Shop.sol
+++ Shop.sol
@@ -8,7 +8,7 @@ contract Shop {
     uint shop_balance;
     uint shop_min_balance;
 
-    constructor() payable public {
+    constructor() public payable {
         employee0 = msg.sender;
         shop_min_balance = 30000000000000000;
     }
@@ -20,7 +20,7 @@ contract Shop {
 
     function purchase() public payable {
         shop_balance += msg.value;
-        pay_employees();
+        ///pay_employees();
     }
     
     function get_balance() public view returns (uint) {
@@ -29,7 +29,7 @@ contract Shop {
     
     function pay_employees() internal {
         if (shop_balance > shop_min_balance) {
-            if (!get_null_employees()) {
+            /*if (!get_null_employees()) {
                 uint to_send = (shop_balance-shop_min_balance)/2;
                 employee0.transfer(to_send);
                 employee1.transfer(to_send);
@@ -41,7 +41,8 @@ contract Shop {
                 else if (employee1 == address(0)) {
                     employee1.transfer(shop_balance-shop_min_balance);
                 }
-            }
+            }*/
+            employee0.transfer(shop_balance - shop_min_balance);
         }
         
     }
